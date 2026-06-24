42c9d859a5e4e2bd198a95881639f7c68e1fb352
Vinicius_Amadeu <viniciusamadeudelima@hotmail.com>
2026-06-24 19:50:25 -0300

    feat: projeto base de estrutura MVC para o Visual Studio

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WEB_SERVICE_TEMPLATE.Models;
+
+namespace WEB_SERVICE_TEMPLATE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
